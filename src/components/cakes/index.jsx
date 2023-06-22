424735f4f8c8327dd0c216b491e5761f115b1cc9
SmitKotadia <smit.kotadia@gmail.com>
2023-06-22 10:21:39 +0530

    limit set

--- src/components/cakes/index.jsx
+++ src/components/cakes/index.jsx
@@ -5,16 +5,23 @@ const Cakes = () => {
   const noOfItems = useSelector((state) => state);
   const dispatch = useDispatch();
   const { noOfCakesMade, noOfCakesSold } = noOfItems;
+  const available = noOfCakesMade - noOfCakesSold;
 
   return (
     <>
       <p>Cakes made - {noOfCakesMade}</p>
       <p>Cakes sold - {noOfCakesSold}</p>
       <p>Cakes available - {noOfCakesMade - noOfCakesSold}</p>
-      <button onClick={() => dispatch(cakeActions("buy-cake"))}>
+      <button
+        onClick={() => dispatch(cakeActions("buy-cake"))}
+        disabled={!available}
+      >
         Buy Cake
       </button>
-      <button onClick={() => dispatch(cakeActions("make-cake"))}>
+      <button
+        onClick={() => dispatch(cakeActions("make-cake"))}
+        disabled={available >= 40}
+      >
         Make Cake
       </button>
     </>
--- src/components/iceCreams/index.jsx
+++ src/components/iceCreams/index.jsx
@@ -5,16 +5,23 @@ const IceCreams = () => {
   const noOfItems = useSelector((state) => state);
   const dispatch = useDispatch();
   const { noOfIceCreamsMade, noOfIceCreamsSold } = noOfItems;
+  const available = noOfIceCreamsMade - noOfIceCreamsSold;
 
   return (
     <>
       <p>Ice-Creams made - {noOfIceCreamsMade}</p>
       <p>Ice-Creams sold - {noOfIceCreamsSold}</p>
-      <p>Ice-creams available - {noOfIceCreamsMade - noOfIceCreamsSold}</p>
-      <button onClick={() => dispatch(iceCreamActions("buy-ice-cream"))}>
+      <p>Ice-creams available - {available}</p>
+      <button
+        onClick={() => dispatch(iceCreamActions("buy-ice-cream"))}
+        disabled={!available}
+      >
         Buy Ice-Creams
       </button>
-      <button onClick={() => dispatch(iceCreamActions("make-ice-cream"))}>
+      <button
+        onClick={() => dispatch(iceCreamActions("make-ice-cream"))}
+        disabled={available >= 60}
+      >
         Make Ice-Creams
       </button>
     </>
